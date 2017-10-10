14e9ac14d70312f59346a8c49a63099fd2bdf694
奈良　智樹 <niconicosenyouv@gmail.com>
2017-10-10 23:02:21 +0900

    BossTestの自信が無いです。というかやり方が分からない上説明も伝わらなかったので確実に間違っています。よろしくお願いします

---
+++ Assets/Bosstest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss {        
+	private int hp = 100;          // 体力
+	private int power = 25; // 攻撃力
+	private int mp = 53; // 魔力
+	// 攻撃用の関数
+	public void Attack() { 
+		Debug.Log( this.power + "のダメージを与えた" );
+	}
+
+	// 防御用の関数
+	public  void Defence(int damage) { 
+		Debug.Log( damage+"のダメージを受けた" );
+		// 残りhpを減らす
+		this.hp -= damage;
+	}
+	public void Magic( int mp ){
+		this.mp -= mp;
+		if (mp < 0) {
+			Debug.Log ("ＭＰが足りないため魔法が使えない");
+		} else {
+			Debug.Log ("魔法攻撃をした。残りＭＰは" + this.mp);
+		}	
+	}
+}
+
+public class Bosstest : MonoBehaviour {
+
+	void Start () {
+		// Bossクラスの変数を宣言してインスタンスを代入
+		Boss lastboss = new Boss ();
+
+		// 攻撃用の関数を呼び出す
+		lastboss.Attack();
+		// 防御用の関数を呼び出す
+		lastboss.Defence(3);
+		
+		lastboss.Magic(5);
+		for (Magic i = 1; i <= 10; i++) {
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+	void Start () {
+		int[] array = new int[5];
+
+		array [0] = 1;
+		array [1] = 2;
+		array [2] = 3;
+		array [3] = 4;
+		array [4] = 5;
+		for (int i = 4; 0 <= i; i--) {
+			Debug.Log (array [i]);
+		
+		}
+		for (int i = 0; 4 >= i; i++) {
+			Debug.Log (array [i]);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
